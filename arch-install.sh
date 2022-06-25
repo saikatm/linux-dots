55e4bd59b925cf0a1aca8ec6bc1b4af0b5defbda
Saikat <saikatm@users.noreply.github.com>
2022-06-25 23:58:55 +0530

    updated

--- arch-install.sh
+++ arch-install.sh
@@ -6,10 +6,8 @@ sudo pacman -Syu
 # install the apps
 pacman -S --needed base-devel git neofetch ntfs-3g git man-db man-pages gst-libav nano rofi kitty gparted papirus-icon-theme vlc micro clinfo android-tools nomacs firefox flatpak -y
 
-# copy the files
-cd linux-dots;
+# copy the files ## make sure to stay in the linux-dots dir
 cp .config/rofi/ ~/.config/; .config/kitty/ ~/.config/; .config/fish/ ~/.config/; cp .fonts ~/.; fc-cache -fv 
-
 
 # install the aur helper
 cd ..
