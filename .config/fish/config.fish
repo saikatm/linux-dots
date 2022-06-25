1a6f8b6b1148f0362ced0ee3f3b2301e29def5c8
saikatm <saikatm@users.noreply.github.com>
2022-06-26 00:39:29 +0530

    updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,28 +10,34 @@ alias lsl "ls -la --group-directories-first -lh"
 alias search "apt search"
 alias show "apt-cache show"
 alias install "sudo apt install --no-install-recommends"
-alias upgrade "sudo apt upgrade"
-alias update "sudo apt update"
-alias remove "sudo apt remove"
-alias purge "sudo apt purge"
-alias clean "sudo apt-get clean"
-alias autoclean "sudo apt-get autoclean"
-alias autoremove "sudo apt-get autoremove"
-alias reconfigure "sudo dpkg-reconfigure"
-alias pkglist "dpkg --get-selections | grep -v deinstall | sed s/\tinstall//g"
-alias version "apt-cache policy"
 alias font-refresh "fc-cache -fv"
 alias clone "git clone --depth 1"
 alias nf "neofetch"
-alias flu "flatpak update"
-alias gst "git status"
+alias fl-u "flatpak update"
+alias fl-l "flatpak list"
+alias g-st "git status"
 alias ttc "tty-clock -c -t"
 
+## arch based os specific aliases 
+alias update "sudo pacman -Syu"
+alias search "pacman -Ss"
+alias remove "sudo pacman -Rsc"
+alias purge "sudo apt purge"
+alias clean "sudo pacman -Sc"
+
+## debian based OS specifiv aliases 
+#alias upgrade "sudo apt upgrade"
+#alias update "sudo apt update"
+#alias remove "sudo apt remove"
+#alias purge "sudo apt purge"
+#alias clean "sudo apt-get clean"
+#alias autoclean "sudo apt-get autoclean"
+#alias autoremove "sudo apt-get autoremove"
+#alias reconfigure "sudo dpkg-reconfigure
+
+
 # config recomended for bobthe fish theme
-
-
 ## git 
-
 
 set -g theme_display_cmd_duration yes
 set -g theme_title_display_process yes
@@ -56,10 +62,6 @@ set -g fish_prompt_pwd_dir_length 0
 set -g theme_project_dir_length 1
 set -g theme_newline_cursor no
 set -g theme_newline_prompt '$ '
-
-
-
-
 
 
 ## enable starship prompt
