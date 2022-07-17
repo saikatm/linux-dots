aac6842f05dcbb7876adb4ec057f0e8220319fbe
saikatm <saikatm@users.noreply.github.com>
2022-07-17 11:34:31 +0530

    updated

--- install-repo.sh
+++ install-repo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+cd ..;
 ## clone the repos
 git clone https://github.com/saikatm/wallpaperx; 
 git clone https://github.com/saikatm/fonts; 
 git clone https://github.com/saikatm/linux-dots;
 ## installing the fonts
-cd ..;
 cd fonts; cp -r .fonts ~/; cd ..;
 ## installing the wallpapers
 cd wallpaperx; cp -r .wallpapers ~/; cd ..;
