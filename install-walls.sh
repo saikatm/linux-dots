4672c2ba34d9b6dae044bef00f7f6c16971f644d
saikatm <saikatm@users.noreply.github.com>
2022-07-26 18:58:24 +0530

    fixed typo ⌨️

--- install-walls.sh
+++ install-walls.sh
@@ -2,7 +2,7 @@
 echo "going to previous dir" &&
 cd .. &&
 echo "cloning the wallpaper repo" &&
-git clone --depth 1 https://github.com/saikatm/wallpaperx.git && 
+git clone --depth 1 https://github.com/saikatm/wallpaperx.git; 
 echo "moving wallpapers" &&
 cd  wallpaperx && cp -r .wallpapers ~/ && 
 echo "All Wallpapers are copied!";
