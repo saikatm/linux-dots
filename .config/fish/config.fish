695e1c0e239c1100287c1b9f28c71af799f064f8
saikatm <saikatm@users.noreply.github.com>
2022-07-27 01:36:01 +0530

    made some cool changes 💖

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,26 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-##--------global Aliases agnostic to any distribution--------##
+#----- Short aliases are for frequent commands and options-------
+# check here for more: https://github.com/GitAlias/gitalias
+alias g-push "git push -u origin main"
+alias g-a "git add"
+alias g-b "git branch"
+alias g-bm "git branch --merged "
+alias g-bnm "git branch --no-merged "
+alias g-bed "git branch --edit-description"
+alias g-bsd "git commit  branch --show-description"
+alias g-m "git commit -m"
+alias g-co "git checkout"
+alias g-ls "git ls-files"
+alias g-rv "git revert "
+alias g-rvnc "git revert --no-commit"
+alias g-sb "git show-branch"
+alias g-smui "git submodule update --init"
+alias g-s "git status"
+alias g-w "git whatchanged"
+
+#--------global Aliases agnostic to any distribution--------
 alias ls "ls --group-directories-first"
 alias lsl "ls -la --group-directories-first -lh"
 alias font-refresh "fc-cache -fv"
@@ -14,12 +33,7 @@ alias fl-l "flatpak list"
 alias m "micro"
 alias lsd "lsd -la"
 
-##----- git specific alias----------#
-alias g-st "git status"
-alias g-cm "git commit -m"
-alias push "git push -u origin main"
-
-##---------arch based os specific aliases---------## 
+#---------arch based os specific aliases---------
 alias update "sudo pacman -Syu"
 alias search "pacman -Ss"
 alias remove "sudo pacman -Rsc"
