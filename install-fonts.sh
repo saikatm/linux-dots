741912704f03319ab76cac03ea21183c2c2b0df3
saikatm <saikatm@users.noreply.github.com>
2022-07-26 18:49:19 +0530

    fixed script 🔨

--- install-fonts.sh
+++ install-fonts.sh
@@ -2,10 +2,10 @@
 echo "going to previous dir" &&
 cd .. &&
 echo "cloning the font repo" &&
-git clone --depth 1 https://github.com/saikatm/fonts.git && 
+git clone --depth 1 https://github.com/saikatm/fonts.git; 
 cd fonts &&
 echo "moving font files" &&
-cd fonts && cp -r .fonts ~/ && 
+cp -r .fonts ~/ && 
 ## clear font cache
 echo "clear font cache" &&
 fc-cache -fv &&
