4b739a3c54ff216ff59c37117f13f8ac18382091
Saikat <saikatm@users.noreply.github.com>
2022-08-10 20:05:20 +0530

    updated

--- install-apps-debian.sh
+++ install-apps-debian.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
 #update first!
-sudo apt update -y
+sudo apt update -y;
 
-sudo apt install -y papirus-icon-theme gparted rofi kitty ntfs-3g vlc micro clinfo adb nomacs firefox flatpak;
+sudo apt install -y papirus-icon-theme gparted rofi kitty ntfs-3g vlc micro clinfo adb nomacs firefox;
+echo "Basic apps are installed!";
 
 
 ## install virt manger
-echo "Installation done. please reboot."
-
+echo "Installing virt manager..";
 sudo apt install -y qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manager;
 
-## check for hardware support
-LC_ALL=C lscpu | grep Virtualization
 
 
 #fix the permissions 
-echo "fixing permissions!"
+echo "fixing permissions!";
 sudo usermod -G kvm -a $USER;
 sudo usermod -G libvirt -a $USER;
 sudo systemctl enable libvirtd;
 sudo systemctl start libvirtd.service;
 
 # reboot the computer
-echo "Installation done. please reboot!"
+echo "Installation done. please reboot!";
 
