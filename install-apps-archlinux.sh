67486e78ffa4ba37b2d397c79ebd8334d3a1c795
Saikat <saikatm@users.noreply.github.com>
2022-08-11 21:03:26 +0530

    .

--- install-apps-archlinux.sh
+++ install-apps-archlinux.sh
@@ -9,16 +9,16 @@ echo "installing all the apps..";
 
 pacman -S --needed base-devel git neofetch ntfs-3g git man-db man-pages gst-libav nano rofi kitty gparted papirus-icon-theme vlc micro clinfo android-tools nomacs firefox -y;
 
-#install KVM and virt manager.
-echo "Installatiing KVM and Virt Manager.."
-
-sudo pacman -S virt-manager qemu vde2 ebtazbles dnsmasq bridge-utils openbsd-netcat libguestfs iptables ebtables -y;
-
-##fix the permissions 
-sudo usermod -G kvm -a $USER;
-sudo usermod -G libvirt -a $USER;
-sudo systemctl enable libvirtd;
-sudo systemctl start libvirtd.service;
+# #install KVM and virt manager.
+# echo "Installatiing KVM and Virt Manager.."
+# 
+# sudo pacman -S virt-manager qemu vde2 ebtazbles dnsmasq bridge-utils openbsd-netcat libguestfs iptables ebtables -y;
+# 
+# ##fix the permissions
+# sudo usermod -G kvm -a $USER;
+# sudo usermod -G libvirt -a $USER;
+# sudo systemctl enable libvirtd;
+# sudo systemctl start libvirtd.service;
 
 
 # install the aur helper
