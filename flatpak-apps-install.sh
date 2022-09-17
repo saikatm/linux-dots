93f1b6c539e2872fe7f9bb24ea200555eebfb9ed
Saikat <saikatm@users.noreply.github.com>
2022-09-18 01:13:15 +0530

    replaced chromium with google chrome

--- flatpak-apps-install.sh
+++ flatpak-apps-install.sh
@@ -4,7 +4,7 @@ flatpak install flathub com.github.tchx84.Flatseal -y;
 flatpak install flathub com.mattjakeman.ExtensionManager -y;
 flatpak install flathub com.visualstudio.code -y;
 flatpak install flathub net.agalwood.Motrix -y:
-flatpak install flathub org.chromium.Chromium -y;
+flatpak install flathub com.google.Chrome -y;
 flatpak install flathub org.gnome.BreakTimer -y;
 flatpak install flathub org.gnome.clocks -y;
 flatpak install flathub org.librehunt.Organizer -y;
