ae2c9a44cafc18d006cdff6c49bb3ebd5e72546c
Saikat <saikatm@users.noreply.github.com>
2022-10-17 23:41:46 +0530

    removed pikaur

--- archlinux-install-all.sh
+++ archlinux-install-all.sh
@@ -6,12 +6,12 @@ sudo pacman -Sy;
 # install the apps
 echo "installing all the apps..";
 sudo pacman -S --noconfirm - < pkgs-arch.txt;
-# install the aur helper
-echo "installing aur helper pikaur.."
-cd ..;
-git clone https://aur.archlinux.org/pikaur.git;
-cd pikaur; makepkg --noconfirm -fsri;
-cd ..;
+# # install the aur helper
+# echo "installing aur helper pikaur.."
+# cd ..;
+# git clone https://aur.archlinux.org/pikaur.git;
+# cd pikaur; makepkg --noconfirm -fsri;
+# cd ..;
 # install KVM and virt manager.
 echo "Installing kvm & virt manager..";
 sudo pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat ebtables iptables libguestfs -y;
