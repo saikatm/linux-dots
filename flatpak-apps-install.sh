a45b638ff90518ff09a1bcfc501c5cc7863b7e8a
Saikat <saikatm@users.noreply.github.com>
2022-10-17 11:09:42 +0530

    updated 😀

--- flatpak-apps-install.sh
+++ flatpak-apps-install.sh
@@ -11,3 +11,9 @@ flatpak install flathub org.librehunt.Organizer -y;
 flatpak install flathub org.mozilla.firefox -y; 
 flatpak install flathub org.telegram.desktop -y;
 flatpak install flathub net.agalwood.Motrix -y;
+flatpak install flathub com.notesnook.Notesnook -y;
+flatpak install flathub com.rafaelmardojai.Blanket -y;
+flatpak install flathub org.libreoffice.LibreOffice -y;
+flatpak install flathub org.gnome.Solanum -y;
+flatpak install flathub com.dropbox.Client -y;
+
