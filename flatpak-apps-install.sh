ad88e0ad4064becab33aca3b276f970bd543d75e
Saikat <saikatm@users.noreply.github.com>
2023-10-22 23:46:18 +0530

    changed flatpak apps

--- flatpak-apps-install.sh
+++ flatpak-apps-install.sh
@@ -1,19 +1,7 @@
 flatpak install flathub com.discordapp.Discord -y;
 flatpak install flathub com.github.johnfactotum.Foliate -y;
-flatpak install flathub com.github.tchx84.Flatseal -y;
 flatpak install flathub com.mattjakeman.ExtensionManager -y;
-flatpak install flathub com.visualstudio.code -y;
-flatpak install flathub net.agalwood.Motrix -y;
-flatpak install flathub com.google.Chrome -y;
 flatpak install flathub org.gnome.BreakTimer -y;
-flatpak install flathub org.gnome.clocks -y;
-flatpak install flathub org.librehunt.Organizer -y;
-flatpak install flathub org.mozilla.firefox -y; 
-flatpak install flathub org.telegram.desktop -y;
 flatpak install flathub net.agalwood.Motrix -y;
-flatpak install flathub com.notesnook.Notesnook -y;
-flatpak install flathub com.rafaelmardojai.Blanket -y;
-flatpak install flathub org.libreoffice.LibreOffice -y;
-flatpak install flathub org.gnome.Solanum -y;
-flatpak install flathub com.dropbox.Client -y;
-
+flatpak install flathub nz.mega.MEGAsync -y;
+flatpak install flathub com.authy.Authy -y;
